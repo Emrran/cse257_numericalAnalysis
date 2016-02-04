109c27d133d710e51d6e54dfcdee9bdc21f9e859
Shawon Ashraf <shawon@shawon-s-macbook-pro.local>
2016-02-04 21:50:03 +0600

    Finalized

--- Matlab Codes/Lab Assignment 1/stat.m
+++ Matlab Codes/Lab Assignment 1/stat.m
@@ -6,20 +6,20 @@ len_freq = length(f);
 % mean calculation
 
 sum_freq = sum(f);
-mean = sum_freq / len_marks
+mean = sum_freq / len_marks;
 
 % median calculation
 
-if( (len_freq / 2) == 0 ) % if even
+if( mod(len_freq, 2) == 0 ) % if even
     medianIndex = int32((len_freq / 2));
-    median = (f(medianIndex) + f(medianIndex + 1)) / 2
+    median = (f(medianIndex) + f(medianIndex + 1)) / 2;
 else % if odd
     medianIndex = int32((len_freq / 2));
-    median = f(medianIndex)
+    median = f(medianIndex);
 end
 
 %mode calculation
 
-mode = max(f)
+mode = max(f);
 
 end
